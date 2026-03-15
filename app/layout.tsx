57d053b5c9d04aa678e7a3f0125cbe9d9d358a89
X.Ny <87480766+XNyLin@users.noreply.github.com>
2026-03-15 17:14:24 +0800

    add layout

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "心動小鎮圖鑑",
+  description: "Heart Town Encyclopedia"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh-TW">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
